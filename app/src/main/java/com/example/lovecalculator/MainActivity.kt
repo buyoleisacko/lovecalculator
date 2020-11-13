eb6a7e72f0e112111dcfd111da39c103c8c09766
Buyole <buyole.isako@twiga.com>
2020-11-13 19:07:28 +0300

    commit

--- app/src/main/java/com/example/lovecalculator/MainActivity.kt
+++ app/src/main/java/com/example/lovecalculator/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.lovecalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
 }
